c63757b539ddf408efc74c54d218365d71807ce7
Abhijnan Pradhan <abhijnan.pradhan@ibm.com>
2021-03-03 23:20:27 +0530

    header,footer and div align in dashboard

--- Frontend/Team4Project/src/app/header/header.component.ts
+++ Frontend/Team4Project/src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faSearch,faUser,faBookmark } from '@fortawesome/free-solid-svg-icons'
 
 @Component({
   selector: 'app-header',
@@ -7,6 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  faSearch = faSearch;
+  faUser = faUser;
+  faBookmark = faBookmark;
   constructor() { }
 
   ngOnInit(): void {
