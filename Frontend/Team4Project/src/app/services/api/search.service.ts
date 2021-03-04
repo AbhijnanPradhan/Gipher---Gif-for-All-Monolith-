f936d0bc9e2941b087774c411bc6abbb94656482
Abhijnan Pradhan <abhijnan.pradhan@ibm.com>
2021-03-04 16:35:53 +0530

    added ApiDataInterface, Fetch working, Search to be implemented

--- Frontend/Team4Project/src/app/services/api/search.service.ts
+++ Frontend/Team4Project/src/app/services/api/search.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from "@angular/common/http";
+import { Observable } from 'rxjs';
+import { ApiDataInterface } from '../../interfaces/ApiDataInterface'
 @Injectable({
   providedIn: 'root'
 })
 export class SearchService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  apiCaller(): Observable<ApiDataInterface>{
+    return this.http.get<ApiDataInterface>('https://api.giphy.com/v1/gifs/search?api_key=YIjy7FhdwY94RyTHx6qenE65qjGw49Tx&q=winnie&limit=3&offset=0&rating=g&lang=en');
+  }
 }
