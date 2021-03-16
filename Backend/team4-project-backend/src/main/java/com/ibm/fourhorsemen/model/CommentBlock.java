ca4220d1ec8a73a1f3e976ab9dcbc672202681f9
Sayan Tapadar <Sayan.Tapadar@ibm.com>
2021-03-16 12:20:53 +0530

    some fixes in Comments backend

--- Backend/team4-project-backend/src/main/java/com/ibm/fourhorsemen/model/CommentBlock.java
+++ Backend/team4-project-backend/src/main/java/com/ibm/fourhorsemen/model/CommentBlock.java
@@ -1,28 +1,18 @@
 package com.ibm.fourhorsemen.model;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document
 public class CommentBlock {
-	@Id private String commentID;
+	private String commentID;
 	
 	private String userID;
 	private String gifID;
 	private String comment;
-	private long likes;
-	private List<String> likerIDs;
-	private boolean edited;
 	
 	
 	public CommentBlock() {
 		super();
-		this.likes = 0;
-		this.edited = false;
-		this.likerIDs = new ArrayList<String>();
 	}
 	
 	/**
@@ -38,21 +28,6 @@ public class CommentBlock {
 		this.userID = userID;
 		this.gifID = gifID;
 		this.comment = comment;
-		this.likes = 0;
-		this.edited = false;
-		this.likerIDs = new ArrayList<String>();
-	}
-	
-
-	public CommentBlock(String commentID, String userID, String gifID, String comment) {
-		super();
-		this.commentID = commentID;
-		this.userID = userID;
-		this.gifID = gifID;
-		this.comment = comment;
-		this.likes = 0;
-		this.edited = false;
-		this.likerIDs = new ArrayList<String>();
 	}
 
 	public String getCommentID() {
@@ -67,15 +42,7 @@ public class CommentBlock {
 	public String getComment() {
 		return comment;
 	}
-	public long getLikes() {
-		return likes;
-	}
-	public boolean getEdited() {
-		return edited;
-	}
-	public List getLikerIds() {
-		return likerIDs;
-	}
+
 	public void setCommentID(String commentID) {
 		this.commentID = commentID;
 	}
@@ -87,32 +54,5 @@ public class CommentBlock {
 	}
 	public void setComment(String comment) {
 		this.comment = comment;
-	}
-	public void setLikes(long likes) {
-		this.likes = likes;
-	}
-	public void setEdited() {
-		this.edited = true;
-	}
-	public void addLike() {
-		this.likes += 1;
-	}
-	public boolean removeLike() {
-		if(this.likes >= 0)
-			this.likes -= 1;
-		
-		return(this.likes >= 0);
-	}
-	public void addToLikerIds(String likerID) {
-		this.likerIDs.add(likerID);
-	}
-	public boolean removeFromLikerIds(String likerID) {
-		if(this.likerIDs.contains(likerID))
-			this.likerIDs.remove(likerID);
-		
-		return this.likerIDs.contains(likerID);
-	}
-	public boolean likerIDPresent(String likerID) {
-		return this.likerIDs.contains(likerID);
 	}
 }
--- Backend/team4-project-backend/src/main/java/com/ibm/fourhorsemen/repository/CommentDataRepository.java
+++ Backend/team4-project-backend/src/main/java/com/ibm/fourhorsemen/repository/CommentDataRepository.java
@@ -1,12 +1,18 @@
 package com.ibm.fourhorsemen.repository;
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.ibm.fourhorsemen.model.CommentBlock;
+import com.ibm.fourhorsemen.model.ExtendedCommentBlock;
 
 
 @Repository
-public interface CommentDataRepository extends MongoRepository<CommentBlock, String>{
+public interface CommentDataRepository extends MongoRepository<ExtendedCommentBlock, String>{
 
+	@Query("{'userID' : ?0}")
+	List<ExtendedCommentBlock> findCommentsByUserId(String userId);
 }
 
