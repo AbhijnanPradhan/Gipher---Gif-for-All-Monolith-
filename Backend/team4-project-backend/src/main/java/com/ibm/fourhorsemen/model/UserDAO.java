8ba71e810c99d82cb19ba55dd28cac0e3d857d09
Sufiyan Ali <Sufiyan.Ali@ibm.com>
2021-03-17 00:15:16 +0530

    commit

--- Backend/team4-project-backend/src/main/java/com/ibm/fourhorsemen/model/UserDAO.java
+++ Backend/team4-project-backend/src/main/java/com/ibm/fourhorsemen/model/UserDAO.java
@@ -1,5 +1,58 @@
 package com.ibm.fourhorsemen.model;
 
-public class UserDAO {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
+public class UserDAO {
+	@Id
+	@GeneratedValue(strategy =GenerationType.IDENTITY)
+	private long id;
+	@Column
+	private String username;
+	@Column
+	private String password;
+	@Column
+	private String role;
+	
+	public UserDAO() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public UserDAO(long id, String username, String password, String role) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.password = password;
+		this.role = role;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	
+	
 }
