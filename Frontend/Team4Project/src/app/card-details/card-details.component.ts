ee6157a8550f3d19b7614f297d78c0c6ea15b06e
Sayan Tapadar <Sayan.Tapadar@ibm.com>
2021-03-19 02:47:51 +0530

    added removeRecommend in card details

--- Frontend/Team4Project/src/app/card-details/card-details.component.ts
+++ Frontend/Team4Project/src/app/card-details/card-details.component.ts
@@ -6,6 +6,7 @@ import { GifDetailsService } from '../services/database/gif_details/gif-details.
 import { RecommendedService } from '../services/database/recommended/recommended.service';
 import { CommentService } from '../services/database/comment/comment.service';
 import { CommentDataInterface } from '../interfaces/CommentDataInterface';
+import { RouterService } from '../services/router.service';
 
 @Component({
   selector: 'app-card-details',
@@ -21,12 +22,12 @@ export class CardDetailsComponent implements OnInit {
   recommended: boolean = false;
   recommendedCount: number = 0;
 
-  commentString:string='';
-  commentMsg:string='';
+  commentString: string = '';
+  commentMsg: string = '';
 
   constructor(private route: ActivatedRoute, private searchService: SearchService,
     private gifDetailsService: GifDetailsService, private recommendedService: RecommendedService,
-    private commentService: CommentService) { }
+    private commentService: CommentService, private routerService: RouterService) { }
 
 
   ngOnInit(): void {
@@ -36,21 +37,31 @@ export class CardDetailsComponent implements OnInit {
     this.datafiller(this.url);
     this.gifDetailsService.getDetails(this.gifId)
       .subscribe(data => {
-        this.favourite = data.favourite;
-        this.recommended = data.recommended;
-        this.recommendedCount = data.recommendCount;
+        if (data.message === "Success") {
+          console.log("gif-details present", data);
+          this.favourite = data.favourite;
+          this.recommended = data.recommended;
+          this.recommendedCount = data.recommendCount;
+        }
       }, error => {
-
+        if (error.status == 401)
+          this.routerService.routeToLogin();
       })
-    this.recommendedService.getBehaviourSubject().subscribe(data => {
-      this.recommendedCount++;
-      this.recommended = true;
+    this.recommendedService.getRecommendModifySubject().subscribe(data => {
+      if (data === "added") {
+        this.recommendedCount++;
+        this.recommended = true;
+        console.log('Recommended what', data);
+      } else if (data === "removed") {
+        this.recommendedCount--;
+        this.recommended = false;
+      }
     })
   }
 
   datafiller(urlParam: string) {
     this.searchService.gifIdCaller(urlParam).subscribe((data) => {
-      // console.log(data);
+      console.log('GifDetails', data);
       // console.log('url:'+ urlParam);
       this.gifDetails = data;
       console.log("img:" + this.gifDetails.data.images.downsized.url);
@@ -64,24 +75,24 @@ export class CardDetailsComponent implements OnInit {
   addFavourite() {
     //TODO Sufiyan
   }
-  removeFavorite(){
-    
+  removeFavorite() {
+
   }
 
   addRecommended() {
     this.recommendedService.addRecommended(this.gifDetails.data);
   }
-  removeRecommended(){
-    // TODO remove
+  removeRecommended() {
+    this.recommendedService.removeRecommended(this.gifDetails.data);
   }
-  addComment(){
-    if(this.commentString===''){
+  addComment() {
+    if (this.commentString === '') {
       this.commentMsg = "Please type something before posting!";
-    }else{
-      let data : CommentDataInterface = new CommentDataInterface;
-        data.comment = this.commentString;
-        data.gifID = this.gifDetails.data.id;
-        //userId being given inside the service
+    } else {
+      let data: CommentDataInterface = new CommentDataInterface;
+      data.comment = this.commentString;
+      data.gifID = this.gifDetails.data.id;
+      //userId being given inside the service
       this.commentService.addComment(data);
       this.commentMsg = "";
     }
