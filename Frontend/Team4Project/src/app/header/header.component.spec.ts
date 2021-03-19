cb6edf07b9b712f2327e6359bd655ee70bf7618f
Sayan Tapadar <Sayan.Tapadar@ibm.com>
2021-03-20 03:22:53 +0530

    changes

--- Frontend/Team4Project/src/app/header/header.component.spec.ts
+++ Frontend/Team4Project/src/app/header/header.component.spec.ts
@@ -1,22 +1,38 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { HeaderComponent } from './header.component';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
+  let h4: string = ''
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
+      declarations: [HeaderComponent],
+      imports: [
+        RouterTestingModule,
+        HttpClientTestingModule,
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+  });
+  // // use fixture.changes() if you need to detect changes after reload.
+  it('should contain correct title after changes', () => {
+    fixture.detectChanges();
+    expect(h4.textContent).toEqual(`Hello ${component.userName} !`)
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
   });
 
   it('should create', () => {
