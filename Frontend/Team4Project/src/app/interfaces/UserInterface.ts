cbf7bcc8780ba8bda56b7e2abc3c9318b9dbcbe9
Abhijnan Pradhan <abhijnan.pradhan@ibm.com>
2021-03-19 08:32:36 +0530

    profile section with activity log. Comment edit needs to be added

--- Frontend/Team4Project/src/app/interfaces/UserInterface.ts
+++ Frontend/Team4Project/src/app/interfaces/UserInterface.ts
@@ -1,13 +1,12 @@
 export class UserInterface {
 
-    userId: string = 'abij1234';
-    name: string = 'Abhijnan Pradhan';
-    email: string = 'abhijnan@abc.com';
-    gender: string = 'male';
-    phone: string = '1234567890';
-    dateOfBirth: Date = new Date('1996-01-01');
+    userId: string = '';
+    name: string = '';
+    email: string = '';
+    gender: string = '';
+    phone: string = '';
+    dateOfBirth: Date = new Date();
     dateAdded: Date = new Date();
-    password: string = '';
     role:string = 'ROLE_ADMIN';
 
     maker(userId: string, name: string, email: string, gender: string, phone: string, dateOfBirth: Date, dateAdded: Date, password: string) {
@@ -18,15 +17,7 @@ export class UserInterface {
         this.phone = phone;
         this.dateOfBirth = dateOfBirth;
         this.dateAdded = dateAdded;
-        this.password = password;
         this.role = 'ROLE_ADMIN';
-    }
-
-    getPassword():string{
-        return this.password;
-    }
-    setPassword(str:string){
-        this.password = str;
     }
     
 }
--- Frontend/Team4Project/src/app/profile/profile.component.ts
+++ Frontend/Team4Project/src/app/profile/profile.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserInterface } from '../interfaces/UserInterface'
+import { ProfileService } from '../services/database/profile/profile.service';
 
 @Component({
   selector: 'app-profile',
@@ -12,16 +13,32 @@ export class ProfileComponent implements OnInit {
   innerWidth:number = 0;
   innerHeight:number = 0;
   coverImgUrl:string = '';
-  profileAvatar: string = '';
+  // profileAvatar: string = '';
   user:UserInterface=new UserInterface();
-  userAge: number = Math.floor(((Date.now()-this.user.dateOfBirth.getTime())/(1000*60*60*24))/365.25);
+  userAge: number = 0;
   months:Array<string> = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
-  constructor() { }
+  constructor(private profileService:ProfileService) { }
 
   ngOnInit(): void {
     this.innerWidth = Math.floor(window.innerWidth);
-    this.innerHeight = Math.floor((window.innerHeight)/4) ;
+    this.innerHeight = Math.floor((window.innerHeight)*70/100) ;
     this.coverImgUrl = "https://picsum.photos/"+this.innerWidth+"/"+this.innerHeight+"";
-    this.profileAvatar = '../../assets/Images/avatars/hacker.png';
+    // this.profileAvatar = '../../assets/Images/avatars/hacker.png';
+
+    this.profileService.getBehaviourSubject()
+      .subscribe(data => {
+        // console.log('get result', data);
+        this.user = data;
+        this.user.dateOfBirth = new Date(data.dateOfBirth);
+        this.user.dateAdded = new Date(data.dateAdded);
+        this.userAge = Math.floor(((Date.now()-this.user.dateOfBirth.getTime())/(1000*60*60*24))/365.25);
+
+        // console.log('UserData', this.user);
+        // console.log("get date",this.user.dateOfBirth.getDate(),);
+        // console.log("get data type of",data.dateOfBirth.getUTCDate());
+
+      });
+    this.profileService.getUserDetails();
   }
+  
 }
